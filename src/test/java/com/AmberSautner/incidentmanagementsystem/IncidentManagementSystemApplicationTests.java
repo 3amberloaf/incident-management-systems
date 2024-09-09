08b4293cfae0dcd53296b869a21f4667319fbe58
Amber Sautner <ambersautner3@gmail.com>
2024-09-09 13:39:03 -0400

    first commit

---
+++ src/test/java/com/AmberSautner/incidentmanagementsystem/IncidentManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AmberSautner.incidentmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IncidentManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
